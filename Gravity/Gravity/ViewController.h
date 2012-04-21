baac0ce1263914d25f22b8dab73df27c13e1a4a7
David Brown <ddb@bithead.net>
2012-04-20 22:37:11 -0700

    initial

---
+++ Gravity/Gravity/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gravity
+//
+//  Created by David Brown on 4/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
